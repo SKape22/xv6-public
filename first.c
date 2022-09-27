3826ca7aac2c045435064fc1337d6c8ea00ee781
pushkar <pushkar.umrao@gmail.com>
2022-09-27 16:37:41 +0530

    First assignment done

--- first.c
+++ first.c
@@ -5,5 +5,20 @@
 int main(int argc, char const *argv[])
 {
     printf(1,"My First User Program on Xv-6.\n");
+    int size=getprocessmemory();
+    printf(1,"Process memory is %d\n", size);
+
+    int val=34;
+    val--;
+    int *a=(int *)malloc(sizeof(int)*2);
+    a[0]=1;
+    a[1]=0;
+    size=getprocessmemory();
+    printf(1,"Process memory after malloc is %d\n", size);
+
+    free(a);
+
+    size=getprocessmemory();
+    printf(1,"Process memory after free is %d\n", size);    
     exit();
 }
--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,10 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+// return the memory size
+int 
+sys_getprocessmemory(void)
+{
+  return myproc()->sz;
+}
